3f5c5cb1c8109d6291b7daf69478fb60bb839b59
Lotte Wijnants <43333232+lotwij@users.noreply.github.com>
2023-11-04 00:17:39 +0100

    styling card with image

---
+++ components/FlipCard.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import { View, TextInput, StyleSheet, Image, TouchableOpacity, Text} from 'react-native';
+import { Card, Title } from 'react-native-paper';
+
+type Props = {
+    title:string
+}
+
+
+const FlipCard: React.FC <Props> = ( {title}, {plaatje} ) => {
+    return (
+    <Card style={styles.card}>
+        <TouchableOpacity>
+          <View style={styles.container}>
+        <Card.Content>
+            
+            <Title style={styles.title}> {title} </Title>
+            <View style={{ flex:1, width: '100%' , borderRadius : 15}} >
+            <Image style={styles.Image} source={require("../data/franjepoot.jpg")} />
+            <View style={styles.textContainer}>
+              <Text style={styles.text}>
+                {title}
+              </Text>
+            </View>
+            </View>
+            <TextInput/>
+        </Card.Content>
+        </View>
+        </TouchableOpacity>
+        </Card>
+    );
+};
+
+const styles = StyleSheet.create({
+    card: {
+        width: 300,
+        height: 450,
+        backgroundColor : 'white', 
+        justifyContent : 'center', 
+        padding: 0,
+        marginTop:'10vw',
+    },
+    container : {
+        width : '100%',
+        height : 400,
+        marginBottom : 25,
+        borderRadius : 15,
+        backgroundColor : '#FFFFFF',
+        overflow : 'hidden'
+      },
+    Image: {
+        width: '100%',
+        height: 400,
+        borderRadius : 5
+    },
+    textContainer : {
+        padding: 2,
+        flex : 1,
+        alignItems : 'center',
+        justifyContent : 'center'
+      },
+
+    title: {
+        fontWeight : 'bold',
+        fontSize : 20,
+
+    }
+})
+
+export default FlipCard;
