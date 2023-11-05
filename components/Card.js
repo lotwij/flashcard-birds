8e77613222bf830e137da1638a6a4716e4eaeb48
Lotte Wijnants <43333232+lotwij@users.noreply.github.com>
2023-11-05 22:40:19 +0100

    cards are now swipeble

--- components/Card.js
+++ components/Card.js
@@ -1,46 +1,80 @@
-import { StyleSheet, Text, View, Image, Dimensions} from 'react-native'
-import React, { Fragment } from 'react'
+import { StyleSheet, Text, View, Image, Dimensions, Animated} from 'react-native'
+import React, { Fragment, useCallback } from 'react'
 import { LinearGradient } from 'expo-linear-gradient';
 import Choice from './Choice';
 
 
 const {width, height } = Dimensions.get("screen")
 
-const Card = ({ name, image, isFirst }) => {
+const Card = ({ name, image, isFirst, swipe, titlSign, ...rest }) => {
+    
+    const rotate = Animated.multiply(swipe.x,titlSign).interpolate({
+        inputRange: [-100,0,100],
+        outputRange: ['8deg', '0deg', '-8deg']
+    });
+    
     // const isFirst = index == 0;
 
-    const renderChoice = () => {
+         // Animated style for the card with rotation and translation
+         const animatedCardStyle = {
+            transform: [...swipe.getTranslateTransform(), { rotate }]
+        }
+    
+        // Opacity animation for the "goed" button
+        const goedOpacity = swipe.x.interpolate({
+            inputRange: [25, 100],
+            outputRange: [0,1],
+            extrapolate: 'clamp'
+        });
+    
+        // Opacity animation for the "fout" button
+        const foutOpacity = swipe.x.interpolate({
+            inputRange: [-100, -25],
+            outputRange: [1,0],
+            extrapolate: 'clamp'
+        });
+
+    const renderChoice = useCallback(() => {
         return (
             <Fragment>
-                <View
-                    style= {[styles.choiceContainer, styles.goedContainer]}
-                >
+                <Animated.View
+                    style= {[
+                        styles.choiceContainer, 
+                        styles.goedContainer,
+                        { opacity: goedOpacity }
+                    ]}>
                     <Choice type="goed"/>
-                </View>
-                <View
-                    style= {[styles.choiceContainer, styles.foutContainer]}
-                >
+                </Animated.View>
+                <Animated.View
+                    style= {[
+                        styles.choiceContainer, 
+                        styles.foutContainer,
+                        { opacity: foutOpacity }
+                    ]}>
                     <Choice type="fout"/>
-                </View>
+                </Animated.View>
             </Fragment>
         )
-    }
+    }, [goedOpacity, foutOpacity])
     
 
 
   return (
-    <View style={styles.container}>
+    <Animated.View style={[
+        styles.container,
+        isFirst && animatedCardStyle
+        ]} {...rest}>
       <Image source={image} style={styles.image} />
       <LinearGradient
-       colors={['transparent','rgba(0,0,0,0.6)']}
+       colors={['transparent','rgba(0,0,0,0)']}
       style={styles.gradient}
       >
         <View style={styles.birdContainer}>
-            <Text style={styles.name}>{name}</Text>
+            {/* <Text style={styles.name}>{name}</Text> */}
         </View>
       </LinearGradient>
       {isFirst && renderChoice()}
-    </View>
+    </Animated.View>
   )
 }
 
--- src/screens/ActieveSpellen.js
+++ src/screens/ActieveSpellen.js
@@ -1,8 +1,10 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { StyleSheet, View, Text, StatusBar, Animated } from 'react-native';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import { StyleSheet, View, Text, StatusBar, Animated, PanResponder, Dimensions} from 'react-native';
 import { birds as birdsArray } from '../../data/data'
 import Card from '../../components/Card';
 import Footer from '../../components/Footer';
+
+const {width, height } = Dimensions.get("screen")
 
 export default function Details() {
   const [birds, setBirds] = useState(birdsArray);
@@ -11,7 +13,45 @@ export default function Details() {
   const swipe = useRef(new Animated.ValueXY()).current;
   const titlsign = useRef( new Animated.Value(1)).current;
 
-  
+  // panresponder config
+  const panResponder = PanResponder.create({
+    onMoveShouldSetPanResponder: () => true,
+
+    onPanResponderMove: (_, {dx, dy, y0}) =>{
+      swipe.setValue({x:dx, y:dy});
+      titlsign.setValue(y0 > (height *0.9)/2 ? 1: -1)
+    },
+    onPanResponderRelease: (_, {dx, dy}) => {
+      const direction = Math.sign(dx);
+      const isActionActive = Math.abs(dx) > 100;
+
+      if(isActionActive){
+        // swiping off screen
+        Animated.timing(swipe, {
+          duration: 100,
+          toValue: {
+            x: direction * 500,
+            y: dy
+          },
+          useNativeDriver: true
+        }).start(removeTopCard);
+      }else{
+        Animated.spring(swipe, {
+          toValue: {
+            x: 0,
+            y: 0
+          },
+          useNativeDriver: true,
+          friction: 5
+        }).start()
+      }
+    }
+  })
+
+  const removeTopCard = useCallback(()=>{
+    setBirds((prevState)=>prevState.slice(1));
+    swipe.setValue({x:0, y:0});
+  }, [swipe])
 
   useEffect(()=>{
     if(!birds.length){
@@ -26,12 +66,16 @@ export default function Details() {
       {
         birds.map(({name, image},index)=>{
           const isFirst = index == 0;
+          const dragHandlers = isFirst ? panResponder.panHandlers : {};
           return(
             <Card 
               key={name}
               name = {name}
               image = {image}
               isFirst = {isFirst}
+              swipe = {swipe}
+              titlSign = {titlsign}
+              {...dragHandlers}
               />
           )
         }).reverse()
